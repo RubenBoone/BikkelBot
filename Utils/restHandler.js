c5d89841e71b5efd0ea6edae25b4cbcaa3caec84
RubenBoone <ruben.boone01@gmail.com>
2025-02-15 01:43:11 +0100

    fix duplicate commands

--- Utils/restHandler.js
+++ Utils/restHandler.js
@@ -6,7 +6,7 @@ const restHandler = async (token) => {
 
     try {
         console.log('Started refreshing application (/) commands.');
-        await rest.put(Routes.applicationGuildCommands("1214874304114855967", "1022527034699808929"), { body: commands });
+        await rest.put(Routes.applicationCommands("1214874304114855967"), { body: commands });
         console.log('Successfully reloaded application (/) commands.');
     } catch (error) {
         console.error(error);
